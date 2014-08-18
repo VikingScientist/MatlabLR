8478ac4ab4f600717ae19c4a39c1b3b66fa666e0
Kjetil Andre Johannessen <kjetijo@gmail.com>
2014-08-18 18:11:23 +0200

    Added: support for more Matlab versions (2011-2015)

--- cmake/Modules/FindMatlab.cmake
+++ cmake/Modules/FindMatlab.cmake
@@ -1,4 +1,14 @@
-find_program(MATLAB_MEX NAMES mex HINTS /usr/lib/matlab2012a/bin)
+find_program(MATLAB_MEX NAMES mex
+  HINTS "/usr/lib/matlab2011a/bin"
+  "/usr/lib/matlab2011b/bin"
+  "/usr/lib/matlab2012a/bin"
+  "/usr/lib/matlab2012b/bin"
+  "/usr/lib/matlab2013a/bin"
+  "/usr/lib/matlab2013b/bin"
+  "/usr/lib/matlab2014a/bin"
+  "/usr/lib/matlab2014b/bin"
+  "/usr/lib/matlab2015a/bin"
+  "/usr/lib/matlab2015b/bin")
 
 find_program(MATLAB_CC NAMES gcc-4.4 gcc)
 
