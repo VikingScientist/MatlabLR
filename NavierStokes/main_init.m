72473d618fc0a2434e7888be9a7ef3697c81ba7d
Kjetil Andre Johannessen <kjetijo@markov.math.ntnu.no>
2015-05-06 11:50:02 +0200

    Added: more modular main loop, making it ready for more test cases

---
+++ NavierStokes/main_init.m
@@ -0,0 +1,46 @@
+
+%%% add path
+addpath('../lib')
+
+%%% reset timers 
+time_assemble      = 0;
+time_plot          = 0;
+time_postprocess   = 0;
+time_refine        = 0;
+time_makeSpace     = 0;
+time_timeStepping  = 0;
+time_savetofile    = 0;
+time_assemble_wall    = 0;
+time_plot_wall        = 0;
+time_postprocess_wall = 0;
+time_refine_wall      = 0;
+time_makeSpace_wall   = 0;
+time_timeStepping_wall= 0;
+time_savetofile_wall  = 0;
+
+%%% plotting parameters
+plotAll            = false;
+plotSol            = true;
+plotDiv            = false;
+plotIndicators     = false;
+plotStream         = false;
+plotDiscretization = false;
+
+%%% setup problem parameters
+xrange  = [0,1];
+yrange  = [0,1];
+nel     = [12,12];
+p       = Problem.Polynomial_Degree;
+gauss_n = p+2;
+Re           = Problem.Reynolds;% Reynolds number
+my           = 1/Re;       % kinematic viscoscity
+nIterations  = 9;          % number of adaptive refinement iterations
+pressureType = 2;          % pressure boundary conditions (1=none, 2=average, 3=corners)
+penalty      = 5*(p(1)+1); % penalty parameter for weakly enforced boundary conditions
+do_save      = false;      % save figure files to Result folder
+nviz         = 7;          % number of visualization points (pr element)
+f  = @(x,y) [0;0];         % default no external forces (overwrite for anasol cases)
+nwtn_res_tol = 1e-10;
+nwtn_max_it  = 12;
+
+
