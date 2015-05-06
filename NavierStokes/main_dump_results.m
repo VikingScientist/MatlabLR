13fd992f41eeaafa6c7c4956e13445fd128bb541
Kjetil Andre Johannessen <kjetijo@markov.math.ntnu.no>
2015-05-06 13:31:07 +0200

    Added: make movie script

--- NavierStokes/main_dump_results.m
+++ NavierStokes/main_dump_results.m
@@ -3,4 +3,8 @@ if ~exist(Problem.Title, 'dir')
   mkdir(Problem.Title);
 end
 filename = sprintf('%s/p%d%d-re%d-T%d', Problem.Title, Problem.Polynomial_Degree, floor(Problem.Reynolds), floor(Problem.Time_Range(2)));
-save(filename, 'Problem', 'lr', 'lru', 'lrv', 'lrp', 'uAll', 'time');
+lr.save( [filename, '-lr.lr' ]);
+lru.save([filename, '-lru.lr']);
+lrv.save([filename, '-lrv.lr']);
+lrp.save([filename, '-lrp.lr']);
+save(filename, 'Problem', 'uAll', 'time');
