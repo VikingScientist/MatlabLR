5f5d44a120202c3b91e9304a7dddb2ba1c3efde9
Kjetil Andre Johannessen <kjetijo@gmail.com>
2015-08-07 13:30:31 +0200

    Added: different problem sets for backward compatible testing

---
+++ NavierStokes/problem_cavity.m
@@ -0,0 +1,71 @@
+clear; close all;
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%
+% Cavity-drive problem. No slip on all walls except top which has
+% prescribed slip u=1
+%
+%
+%
+%           ---->  u=1
+%         +-------------+
+%         |             |
+%     no  |             |  no
+%    slip |   Omega     | slip    
+%         |             |
+%         |             |
+%         +-------------+
+%            no slip                                 
+%                                                       
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+p  = 3; % polynomial degree. May be needed in Problem.Time_Step
+Re = 1; % Reynolds number.   May be needed in Problem.Time_Step
+
+Problem = struct(...
+'Title'             ,  'Cavity',  ...
+'Subtitle'          ,  'linear',   ...
+'Identifier'        ,  'a',        ...
+'Geometry'          ,  'Square',   ...
+'Geometry_param'    ,  1,          ...
+'Polynomial_Degree' ,  [p,p],      ...
+'H_Max'             ,  1/8,        ...
+'H_Min'             ,  1/32,        ...
+'Reynolds'          ,  Re,         ...
+'Geom_TOL'          ,  1e-10,      ...
+'Newton_TOL'        ,  1e-10,      ...
+'Newton_Max_It'     ,  12,         ...
+'Force'             ,  @(x,y)[0,0],...
+'Static'            ,  true,       ...
+'Linear'            ,  true,       ...
+'Paraview'          ,  false,      ...
+'MatlabPlot'        ,  true,       ...
+'Save_Results'      ,  false,       ...
+'Time_Step'         ,  .10,        ...
+'Time_Range'        ,  [0,10]);
+% 'Time_Step'         ,  @(h) min(h^((p+1)/2), h^2 /4*Re), ...
+
+BC     = cell(1);
+BC{1}  = struct('start', [0,0], 'stop', [1,0], 'comp', 2, 'value', 0);
+BC{2}  = struct('start', [0,1], 'stop', [1,1], 'comp', 2, 'value', 0);
+BC{3}  = struct('start', [0,0], 'stop', [0,1], 'comp', 1, 'value', 0);
+BC{4}  = struct('start', [1,0], 'stop', [1,1], 'comp', 1, 'value', 0);
+BC{5}  = struct('start', [0,0], 'stop', [1,0], 'comp', 1, 'value', 0);
+BC{6}  = struct('start', [0,1], 'stop', [1,1], 'comp', 1, 'value', 1);
+BC{7}  = struct('start', [0,0], 'stop', [0,1], 'comp', 2, 'value', 0);
+BC{8}  = struct('start', [1,0], 'stop', [1,1], 'comp', 2, 'value', 0);
+BC{9}  = struct('start', [0,0], 'stop', [0,0], 'comp', 3, 'value', 0);
+BC{10} = struct('start', [1,0], 'stop', [1,0], 'comp', 3, 'value', 0);
+BC{11} = struct('start', [0,1], 'stop', [0,1], 'comp', 3, 'value', 0);
+BC{12} = struct('start', [1,1], 'stop', [1,1], 'comp', 3, 'value', 0);
+
+main_init;
+main_assemble;
+
+if Problem.Static
+  main_static;
+else 
+  main_time_loop;
+end
+
+main_dump_results;
